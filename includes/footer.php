8cc5a39d05e0510039018603b1a3eb983c82b859
Barasbi <web_masters_07@mail.ru>
2017-03-27 23:03:04 +0300

    empty

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,17 @@
-<section class="section section_padding">
+<section class="section section_padding section_footer_view">
 <div class=" container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-3">
+		<span class="db-footer-one">www.stollerukraine.com.ua</span>
+		</div>
+		<div class="col-md-3">
+		<span class="db-footer-two">info@stollerukraine.com</span>
+		</div>
+		<div class="col-md-3">
+		<span class="db-footer-three">+(380) 44-498-52-35</span>
+		</div>
+		<div class="col-md-3">
+		<span class="db-footer-three">+(380) 44-498-52-35</span>
 		</div>
 	</div>
 </div>
--- includes/header-top.php
+++ includes/header-top.php
@@ -1,8 +1,18 @@
-<section class="section section_padding">
+<section class="section section_padding section_header_view">
 <div class=" container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-4">
+		<img src="img/header_top.png" alt="img" class="img-responsive db-header-logo">
 		</div>
+		<div class="col-md-4">
+		<span class="db-header_tel">+ (380) 44-498-52-35</span><br>
+		<span class="db-header_tel">+ (380) 44-498-52-35</span>
+		</div>
+		<div class="col-md-4">
+		<a href="#" class="button_view button_view__header db-header-button">заказать звонок</a><br>
+		<span class="db-header-mess">info@stollerukraine.com</span>
+		</div>
+		<div class="clearfix"></div>
 	</div>
 </div>
 </section>
