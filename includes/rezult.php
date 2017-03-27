1ae031868107242ec5a78683785a774418783d41
Barasbi <web_masters_07@mail.ru>
2017-03-27 23:45:21 +0300

    empty

--- includes/rezult.php
+++ includes/rezult.php
@@ -1,8 +1,16 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding section_result_view">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-6">
+			<p class="result-db-p"><span>результат</span><br>
+					всходы
+				</p>
+			</div>
+			<div class="col-md-6">
+				<p class="result-db-p"><span>результат</span><br>
+					восстановление после града
+				</p>
+			</div>
 		</div>
 	</div>
-</div>
 </section>
