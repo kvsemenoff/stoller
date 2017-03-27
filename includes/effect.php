8e45b817cb47fe458584f6ee25888518ba137edd
Berzeg07 <berezg86@gmail.com>
2017-03-27 20:11:56 +0400

    29.14

--- includes/effect.php
+++ includes/effect.php
@@ -1,8 +1,37 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section>
+	<div class="container dd-form_wrap">
+		<div class="row">
+
+			<div class="col-md-12">
+				<div class="dd-title_form">
+					<p>Рассчитать эффект от использования Stoller<br>
+						на собственном хозяйстве?
+					</p>
+				</div>	
+			</div>
+			<div class="clearfix"></div>
+
+			<div class="form_container">
+
+				<form class="" action="#" method="post">
+					<div class="col-md-3">
+						<input class="input_view input_view__name" name="uname" type="text" placeholder="Имя">
+					</div>
+					<div class="col-md-3">
+						<input class="input_view input_view__phone phone" name="phone" type="text" placeholder="Телефон">	
+					</div>
+					<div class="col-md-3">
+						<input class="input_view input_view__email" name="email" type="text" placeholder="E-mail">	
+					</div>
+					<div class="col-md-3">
+						<input class="button_view" type="submit" name="submit" value="отправить">
+					</div>
+					<div class="clearfix"></div>
+				</form>
+
+			</div>
+
 		</div>
 	</div>
-</div>
+
 </section>
--- includes/sostav.php
+++ includes/sostav.php
@@ -1,8 +1,19 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section>
+	<div class="container dd-sostav">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="dd-left_sostav">
+					<p>
+						В состав препарата входит<br>
+						запатентованное соеденение -<br>
+						антиоксидант - диформил мочевина
+					</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				
+			</div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
-</div>
 </section>
