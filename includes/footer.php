ae6751c54e97ebdac08d0cc5e10965b6cd191db3
kvsemenoff <kvsemenoff@mail.ru>
2017-03-27 15:56:13 +0400

    f

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+<script src="libs/owl.carousel/owl.carousel.js"></script>
+<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script src="js/responsiveTabs.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/common.js"></script>
