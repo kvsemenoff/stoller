ed860843b30ade3130ce09a461b186120aea09c1
Berzeg07 <berezg86@gmail.com>
2017-03-27 19:36:43 +0400

    29.14

--- tech.php
+++ tech.php
@@ -1,19 +1,31 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
-<br><br>		
-<a href="#" class="button-1">Заказать звонок</a>	
-<br><br>	
-<h1>Пример заголовка</h1>
-<br><br>	
-<form action="#">
-	<input type="text" value="" placeholder="Инпут тайп текст">		
-	<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">		
-</form>		
+<section class="tech-wrap">
+
+	<br><br>
+
+	<a href="#" class="button_view">Отправить</a>	
+	<a href="#" class="button_view button_view__header">Заказать звонок</a>	
+	<a href="#" class="button_view button_view__footer">Заказать звонок</a>	
+
+	<br><br>	
+	<h1>Пример заголовка</h1>
+	<br><br>
+
+	<form action="#">
+		<input class="input_view input_view__name" type="text" placeholder="Имя">		
+		<input class="input_view input_view__phone" type="text" placeholder="Телефон">		
+		<input class="input_view input_view__email" type="text" placeholder="E-mail">		
+		<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">		
+	</form>	
+
+	<br><br>
+
+</section>
 
 
-<?php require_once('includes/footer.php'); ?>
 
-	
+
 </body>
 </html>
 
