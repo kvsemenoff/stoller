ad015e64fdcb8af8796ea6cd5d9b714dbd93c52c
Barasbi <web_masters_07@mail.ru>
2017-03-28 10:46:11 +0300

    empty

--- includes/stress.php
+++ includes/stress.php
@@ -1,8 +1,12 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding section_sress_view">
+	<div class=" container">
+		<div class="row">
+			<div class="db-stress-box">
+				<p class="db-stress-p1">Стрессу нет!</p>
+					<div class="db-stress-fon1"></div>
+					<div class="clearfix"></div>
+			</div>
+			
 		</div>
 	</div>
-</div>
 </section>
