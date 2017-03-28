82511f0eda5c4be49937ea1eef1434b682953e32
Berzeg07 <berezg86@gmail.com>
2017-03-28 16:15:01 +0400

    29.14

--- includes/green.php
+++ includes/green.php
@@ -1,16 +1,75 @@
-<section class="section section_padding">
+<section class="section section_padding dd-green-wrap">
 	<div class=" container">
 		<div class="row">
-			<div class="col-md-4">
+
+			<div class="col-md-3">
 				<div class="dd-box_green">
 					<div class="dd-green-img">
-						
+						<img class="img-responsive" src="img/g1.png" alt="">
 					</div>	
 					<div class="dd-green-txt">
-						<p></p>
+						<p>Обеспечивают<br> профилактику стресса</p>
 					</div>
 				</div>
 			</div>
+
+			<div class="col-md-6">
+				<div class="dd-box_green">
+					<div class="dd-green-img">
+						<img class="img-responsive" src="img/g2.png" alt="">
+					</div>	
+					<div class="dd-green-txt">
+						<p>Способствует быстрому восстановлению<br> растений после повреждений</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3">
+				<div class="dd-box_green">
+					<div class="dd-green-img">
+						<img class="img-responsive" src="img/g3.png" alt="">
+					</div>	
+					<div class="dd-green-txt">
+						<p>Оказывает анти-<br> оксидантное воздействие</p>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+
+			<div class="col-md-3">
+				<div class="dd-box_green">
+					<div class="dd-green-img dd-green-img4">
+						<img class="img-responsive" src="img/g4.png" alt="">
+					</div>	
+					<div class="dd-green-txt">
+						<p>Поддерживает оптимальный<br> гормональный баланс</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="dd-box_green">
+					<div class="dd-green-img">
+						<img class="img-responsive" src="img/g5.png" alt="">
+					</div>	
+					<div class="dd-green-txt">
+						<p>Способствует постоянному<br> росту корневой системы</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3">
+				<div class="dd-box_green">
+					<div class="dd-green-img">
+						<img class="img-responsive" src="img/g6.png" alt="">
+					</div>	
+					<div class="dd-green-txt">
+						<p>Повышает устойчивость<br> растений к засухе</p>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+
 		</div>
 	</div>
 </section>
