d9e19dbd51e6c95382e1aba88074d3721155e49b
Barasbi <betokov93@gmail.com>
2017-03-29 15:08:36 +0300

    add

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,4 @@
 </head>
 <body>
 	
-	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
-	<script src="js/common.js"></script>
-	<script src="js/jquery.maskedinput.min.js"></script>
-	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	
